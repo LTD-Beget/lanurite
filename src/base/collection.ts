0a6eaf7a8044300b1e07262d859507e5a88b7fbb
Yura Panarin <ypanarin@beget.ru>
2017-09-05 15:16:42 +0300

    add method GetAll

--- src/base/collection.ts
+++ src/base/collection.ts
@@ -1,14 +1,15 @@
 import {baseModel} from "./model";
 import {baseEvents} from "./events";
 import {Dictionary} from "lodash";
-export interface baseCollection extends baseEvents{
+export interface baseCollection extends baseEvents {
     add(model: baseModel): boolean
     remove(model: baseModel): boolean
     clear(): void
     has(model: baseModel): boolean
+    getAll(): Array<any>
     filter(predicate: any): Array<any>
-    map(predicate:any): Array<any>
-    reduce(predicate:any, accum: any): any
-    getById(id: string): baseModel|null
-    find(predicate, start: Number): any|undefined
+    map(predicate: any): Array<any>
+    reduce(predicate: any, accum: any): any
+    getById(id: string): baseModel | null
+    find(predicate: any, start: Number): any | undefined
 }
--- src/type/Collections.d.ts
+++ src/type/Collections.d.ts
@@ -4,15 +4,16 @@ import { EventsLr } from "../events/Events";
 declare class Collections extends EventsLr implements baseCollection {
     private _models;
     constructor(array?: Array<any>);
-    _init(array: any): void;
+    _init(array: Array<any>): void;
     add(model: baseModel): boolean;
     remove(model: baseModel): boolean;
     has(model: baseModel): boolean;
     clear(): void;
     filter(predicate: any): {}[];
     map(predicate: any): {}[];
-    getById(id: any): any;
+    getById(id: string): any;
     find(predicate: any, startIndex?: number): {};
-    reduce(predicate: any, accum?: number): {};
+    reduce(predicate: any, accum?: any): {};
+    getAll(): {}[];
 }
 export { Collections };
