58e118184bce2924952de3fe3b70fec8066fb5c9
Yura Panarin <tduble94@gmail.com>
2017-09-05 09:35:31 +0300

    add lodash import

--- src/lanurite.ts
+++ src/lanurite.ts
@@ -1,9 +1,11 @@
 import {Models} from "./type/Models";
 import {Collections} from "./type/Collections";
+import * as _ from "lodash";
 
 declare const __VERSION__: string;
 export = {
     models: Models,
     collections: Collections,
-    version: __VERSION__
+    version: __VERSION__,
+    lodash: _
 };
